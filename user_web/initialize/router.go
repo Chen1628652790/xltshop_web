db56d76d77ee6d60c5046788e1828923769dfd0d
xiaolatiao <1628652790@qq.com>
2021-08-31 21:06:00 +0800

    feat:cors跨域

--- user_web/initialize/router.go
+++ user_web/initialize/router.go
@@ -3,11 +3,13 @@ package initialize
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/xlt/shop_web/user_web/middleware"
 	"github.com/xlt/shop_web/user_web/router"
 )
 
 func InitRouter() *gin.Engine {
 	engine := gin.Default()
+	engine.Use(middleware.Cors())
 
 	v1Group := engine.Group("/u/v1")
 	router.InitUserRouter(v1Group)
